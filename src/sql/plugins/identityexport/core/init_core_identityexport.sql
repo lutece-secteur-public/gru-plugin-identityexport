41da825a01a11ea17094306ec9ea0dd65ca9da9c
seboo <25958061+seboo@users.noreply.github.com>
2025-02-17 15:41:11 +0100

    LUT-28459 : update right level

--- src/sql/plugins/identityexport/core/init_core_identityexport.sql
+++ src/sql/plugins/identityexport/core/init_core_identityexport.sql
@@ -4,7 +4,7 @@
 --
 DELETE FROM core_admin_right WHERE id_right = 'IDENTITYEXPORT_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('IDENTITYEXPORT_MANAGEMENT','identityexport.adminFeature.ManageExtraction.name',1,'jsp/admin/plugins/identityexport/ManageProfils.jsp','identityexport.adminFeature.ManageExtraction.description',0,'identityexport',NULL,NULL,NULL,4);
+('IDENTITYEXPORT_MANAGEMENT','identityexport.adminFeature.ManageExtraction.name',2,'jsp/admin/plugins/identityexport/ManageProfils.jsp','identityexport.adminFeature.ManageExtraction.description',0,'identityexport',NULL,NULL,NULL,4);
 
 
 --
